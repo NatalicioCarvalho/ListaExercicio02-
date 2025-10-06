d60b01f4045a5a80ab7eda2db2b04627b9040db0
NatalicioCarvalho <nalsantoscarvalho@gmail.com>
2025-10-06 15:40:58 -0300

    Resposta questão2

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+nome1 = (input("Digite o primeiro nome:"))
+nome2 = (input("Digite o segundo nome:"))
+nome3 = (input("Digite o terceiro nome:"))
+nome4 = (input("Digite o quarto nome:"))
+nome5 = (input("Digite o quinto nome:"))
+print(nome1, nome2, nome3, nome4, nome5, sep=", ")
