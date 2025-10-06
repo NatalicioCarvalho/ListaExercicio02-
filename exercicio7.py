bb80dd36c21978fceea35d1ad30c6393471eae5f
NatalicioCarvalho <nalsantoscarvalho@gmail.com>
2025-10-06 15:49:13 -0300

    Resposta questão7

---
+++ exercicio7.py
@@ -0,0 +1,5 @@
+alimento1 = input("Alimento 1: ")
+alimento2 = input("Alimento 2: ")
+alimento3 = input("Alimento 3: ")
+print(f"Você escolheu levar: {alimento1}, {alimento2} e {alimento3} para a ilha.")
+
