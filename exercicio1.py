7ccc8cb206ae849e5a15bdc70ccd68fc6842dc7d
NatalicioCarvalho <nalsantoscarvalho@gmail.com>
2025-10-06 15:39:05 -0300

    Resposta da questão1

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+numero1 = int(input("Digite o primeiro número:"))
+numero2 = int(input("Digite o segundo número:"))
+numero3 = int(input("Digite o terceiro número:"))
+print(numero1, numero2, numero3)
